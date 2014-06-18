25752c8fe2996a629014722d3cbfda51fe04d92c
David <DevilDavidWang@gmail.com>
2014-06-18 22:05:19 +0800

    add changes

--- logic.js
+++ logic.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 	init();
+	var clock = $('.mystery-clock').FlipClock({
+		clockFace: 'DailyCounter',
+	});
+	var date = new Date(2013, 9, 12, 22,6,10,0);
+	var now = new Date();
+	var seconds = Math.round((now.getTime() - date.getTime()) / 1000);
+	clock.setTime(seconds);
 });
 
 $(window).on('resize', function() {
